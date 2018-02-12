bb6a90ba7c823c32fff46c47fea70830b1e11e69
pbelamge <pbelamge@gmail.com>
2018-02-12 07:52:25 -0500

    tuned initEnv script

--- initEnv.sh
+++ initEnv.sh
@@ -22,6 +22,17 @@ function exit_on_error {
     exit $2
 }
 
+function setIfConfig {
+    info "setting static ip"
+    grep -n "$NODE_IP" /etc/network/interfaces
+    if [[ $? -ne 0 ]]
+    then
+        sudo sed -i 's/enp0s8 inet dhcp/enp0s8 inet static/g' /etc/network/interfaces
+        echo "	address $NODE_IP" | sudo tee -a /etc/network/interfaces
+        echo "  netmask $NODE_MASK" | sudo tee -a /etc/network/interfaces
+    fi
+}
+
 function setHostsInfo {
     info "setting hosts info"
     . setHostsInfo.sh || exit_on_error "$BASH_SOURCE: hosts/name update failed" $?
@@ -36,8 +47,6 @@ function upgradeUbuntu {
 #[[ "$(id -u)" != "0" ]] && exit_on_error "$BASH_SOURCE: This script must be run as root" 1
 
 info "===== ~: $BASH_SOURCE :~ ====="
-
-[[ $FORCE_DEPLOY || $OS_UPGRADE -eq 1 ]] && upgradeUbuntu
 
 info "~: Envs Set :~"
 info "IP: "$NODE_IP
@@ -67,7 +76,11 @@ echo "export no_proxy='$NOPROXY_ADDRESS'" | tee -a ~/.proxyrc
 info "copying custom bashrc"
 cp bashrc ~/.bashrc
 
+[[ $FORCE_DEPLOY || $SKIP_IF_CONFIG -ne 1 ]] && setIfConfig
+
 [[ $FORCE_DEPLOY || $SKIP_ETC_HOSTS -ne 1 ]] && setHostsInfo
+
+[[ $FORCE_DEPLOY || $OS_UPGRADE -eq 1 ]] && upgradeUbuntu
 
 info "===== ~: $BASH_SOURCE - Successful :~ ===== "
 info ""
--- setHostsInfo.sh
+++ setHostsInfo.sh
@@ -6,11 +6,15 @@ DATE='date +%Y/%m/%d:%H:%M:%S'
 PROGNAME=`basename "$0"`
 
 info "===== ~: $BASH_SOURCE :~ ====="
-
-lineNum="$(grep -n "IPv6 capable hosts" /etc/hosts | cut -f1 -d:)"
-atLine=$(($lineNum-1))
-info "Adding '$NODE_IP $NODE_NAME' at line: $atLine"
-sudo sed -i "${atLine}i$NODE_IP	$NODE_NAME" /etc/hosts || { retVal=$?; error "setHostsInfo: hosts update failed! returning..."; return $retVal; }
+grep -n "$NODE_IP" /etc/hosts
+if [[ $? -ne 0 ]]
+then
+    lineNum="$(grep -n "IPv6 capable hosts" /etc/hosts | cut -f1 -d:)"
+    atLine=$(($lineNum-1))
+    info "Adding '$NODE_IP $NODE_NAME $NODE_DOM' at line: $atLine"
+    sudo sed -i "${atLine}i$NODE_IP	$NODE_NAME	$NODE_DOM" /etc/hosts \
+        || { retVal=$?; error "setHostsInfo: hosts update failed! returning..."; return $retVal; }
+fi
 
 info "Updating hostname to '$NODE_NAME' in /etc/hostname"
 echo "$NODE_NAME" | sudo tee /etc/hostname
